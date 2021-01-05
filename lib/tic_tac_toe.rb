2e1964c385d0560ba374a979ab5c02249568e383
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-05 00:09:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,7 +29,7 @@ class TicTacToe
 
   def valid_move?(index)
     #return true if BOTH position taken are false and the move is possible (1-9)
-    true
+    index.to_i.between?(1,9) && !position_taken?(index.to_i)
   end
 
   def turn_count
